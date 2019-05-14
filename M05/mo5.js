41a4355017c8e5daa1512c1d1ebb587463e28aa4
cadecabde <49352789+cadecabde@users.noreply.github.com>
2019-05-14 13:46:02 -0700

    Update mo5.js
    js

--- M05/mo5.js
+++ M05/mo5.js
@@ -1,13 +1,12 @@
-var table = prompt("Enter a value to multiply,1);                      // Unit of table
+var table = prompt("Enter a value to multiply,1);         //Unit of table
 var operator = 'multiplication';          // Type of calculation (defaults to addition)
 var i = 1;                          // Set counter to 1
 var msg = '';                       // Message
 
-if (operator === 'addition') {    
-    // Do addition
- //   while (i < 11) {          // msg += i + ' + ' + table + ' = ' + (i + table) + '<br>';      // Calculation
-    // Add 1++
-//} else {
+if (operator === 'addition') {    // Do addition
+   while (i < 11) {          // msg += i + ' + ' + table + ' = ' + (i + table) + '<br>';      // Calculation
+    Add 1++
+} else {
   // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
