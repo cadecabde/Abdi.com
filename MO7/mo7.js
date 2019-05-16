2af8704506bcf36bbed0d240c4cdb7223012230c
cadecabde <49352789+cadecabde@users.noreply.github.com>
2019-05-16 15:01:55 -0700

    Update mo7.js
    room

--- MO7/mo7.js
+++ MO7/mo7.js
@@ -12,7 +12,7 @@ function Hotel(name, rooms, booked) {
 // Create three hotel objects
 var quayHotel = new Hotel('Quay', 40, 25);
 var parkHotel = new Hotel('Park', 120, 77);
-var SunsetHotel = new Hotel('Sunset', 56, 10);
+var SunsetHotel = new Hotel('Sunset', 86, 10);
 
 
 
