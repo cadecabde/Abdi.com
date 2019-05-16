c3583788d75099a22d733d7de3d973fce78603dc
cadecabde <49352789+cadecabde@users.noreply.github.com>
2019-05-16 13:52:57 -0700

    Update mo5.js
    dd

--- M05/mo5.js
+++ M05/mo5.js
@@ -1,11 +1,11 @@
-var table = 3;                      // Unit of table
+var table = 5;                      // Unit of table
 var operator = 'addition';          // Type of calculation (defaults to addition)
 var i = 1;                          // Set counter to 1
-var msg = '';                       // Message
+var msg = '<h2>Multiplication Table</h2>';                       // Message
 
-if (operator === 'addition') {      // If the operator variable says addition
+if (operator === 'Multiplication ') {      // If the operator variable says addition
     while (i < 11) {                // While counter is less than 11
-        msg += i + ' + ' + table + ' = ' + (i + table) + '<br>';      // Calculation
+        msg += i + ' x ' + table + ' = ' + (i + table) + '<br>';      // Calculation
         i++;                        // Add 1 to the counter
     }
 } else {                            // Otherwise
