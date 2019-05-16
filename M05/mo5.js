d9979d37b0351398e0333dc34466a50f6e5d02a4
cadecabde <49352789+cadecabde@users.noreply.github.com>
2019-05-16 13:40:33 -0700

    Update mo5.js
    w

--- M05/mo5.js
+++ M05/mo5.js
@@ -1,20 +1,18 @@
-var num = prompt("Enter Number","") //prompt user to enter the number
-var operator= 'addition'; // Type of calculation
-var i = 3;                 // Set counter to 1
-var msg = '';              // Message
+var table = 3;                      // Unit of table
+var operator = 'addition';          // Type of calculation (defaults to addition)
+var i = 1;                          // Set counter to 1
+var msg = '';                       // Message
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
+if (operator === 'addition') {      // If the operator variable says addition
+    while (i < 11) {                // While counter is less than 11
+        msg += i + ' + ' + table + ' = ' + (i + table) + '<br>';      // Calculation
+        i++;                        // Add 1 to the counter
+    }
+} else {                            // Otherwise
+    while (i < 11) {                // While counter is less than 11
+        msg += i + ' x ' + table + ' = ' + (i * table) + '<br>';      //Calculation
+        i++;                        // Add 1 to the counter
+    }
 }
 
 // Write the message into the page
