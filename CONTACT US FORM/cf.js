1a3d25d6822ee06c7679f78479a10949c4f884ff
cadecabde <49352789+cadecabde@users.noreply.github.com>
2019-06-19 21:02:47 -0700

    Update cf.js

--- CONTACT US FORM/cf.js
+++ CONTACT US FORM/cf.js
@@ -24,13 +24,13 @@ function validateForm() {
   var validZipcode = false;
 
   if (document.getElementById("firstname").value.length > 0 &&
-      document.getElementById("firstname").value.length <= 20)
+    document.getElementById("firstname").value.length <= 20)
     validFirstname = true;
   else
     errorMessages += "<p> The firstname must be less than or equal to 20 charaters";
 
   if (document.getElementById("lastname").value.length > 0 &&
-      document.getElementById("lastname").value.length <= 50)
+    document.getElementById("lastname").value.length <= 50)
     validLastname = true;
   else
     errorMessages += "<p> The lastname must be less than or equal to 50 characters";
@@ -39,48 +39,43 @@ function validateForm() {
     validEmail = true;
 
   if (myContact.phone.value == null ||
-      myContact.phone.value === "" ||
-      myContact.phone.value.length > 15 ||
-      !myContact.phone.value.match(numbers))
+    myContact.phone.value === "" ||
+    myContact.phone.value.length > 15 ||
+    !myContact.phone.value.match(numbers))
     errorMessages += "<p>The phone number must be less or qual to 15 characters.  Only numbers are accepted.</p>";
   else
     validPhone = true;
 
   if (document.getElementById("username").value.length > 0 &&
-      document.getElementById("username").value.length <= 12)
+    document.getElementById("username").value.length <= 12)
     validUsername = true;
   else
     errorMessages += "<p> The Username must be less than or equal to 12 characters";
 
 
   if (document.getElementById("password").value.length > 0 &&
-      document.getElementById("password").value.length <= 12)
+    document.getElementById("password").value.length <= 12)
     validPassword = true;
   else
     errorMessages += "<p> The Password must be less than or equal to 7 characters";
 
 
   if (myContact.address.value == null ||
-      myContact.address.value === "")
+    myContact.address.value === "")
     errorMessages += "<p> An Address is required</p>";
   else
     validAddress = true;
 
 
   if (myContact.city.value == null ||
-      myContact.city.value === "")
+    myContact.city.value === "")
     errorMessages += "<p> A City is required</p>";
   else
     validCity = true;
 
-  if (myContact.state.value == null ||
-      myContact.state.value === "")
-    errorMessages += "<p> A State is required</p>";
-  else
-    validCountry = true;
 
   if (myContact.countries.value == null ||
-      myContact.countries.value === "")
+    myContact.countries.value === "")
     errorMessages += "<p> A Country is required</p>";
   else
     validCountry = true;
@@ -88,8 +83,8 @@ function validateForm() {
   if (myContact.countries.value == 3)
     if (myContact.zipcode.value.length === 5)
       validZipcode = true
-  else
-    errorMessages += "<p>A Zip Code is required if the chosen country is USA.</p>";
+    else
+      errorMessages += "<p>A Zip Code is required if the chosen country is USA.</p>";
 
 
   console.log("errorMessage");
